a9719051a11f98c3a811d663b5e3d4e31e2255b3
LAB. AULAS A557 <LAB_A557@LABSUCAB.UCAB.EDU.VE>
2026-06-12 12:04:46 -0400

    Parte grafica realizada

--- src/main/java/com/fitmanage/smartgym/controller/GymController.java
+++ src/main/java/com/fitmanage/smartgym/controller/GymController.java
@@ -1,39 +1,28 @@
 package com.fitmanage.smartgym.controller;
 
-import com.fitmanage.smartgym.model.RegistroCentral;
-import com.fitmanage.smartgym.model.SensorHilo;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
 
 public class GymController {
-
-    // Variables enlazadas automáticamente con el archivo FXML mediante sus fx:id
     @FXML private Label labelTemp;
     @FXML private Label labelHum;
-    @FXML private Pane panelAlerta;
+    @FXML private Label panelAlerta;
+    @FXML private Button btnIniciar;
 
-    private RegistroCentral modelo;
-
-    @FXML
-    public void initialize() {
-        // Inicialización del recurso crítico compartido
-        this.modelo = new RegistroCentral();
-
-        // TODO: Issue #3 - Instanciar las tareas de los sensores climáticos
-        // SensorHilo tareaTemp = new SensorHilo("TEMPERATURA", modelo, this);
-        // SensorHilo tareaHum = new SensorHilo("HUMEDAD", modelo, this);
-
-        // TODO: Issue #3 - Crear los objetos de tipo Thread correspondientes y arrancar su ejecución (.start())
+    /**
+     * 2. Método enlazado a la acción del botón de la vista (onAction)
+     */
+    @FXML public void onIniciarClick(ActionEvent event) {
+        System.out.println("¡Botón presionado correctamente!");
+        // Aquí es donde el Integrante 2 iniciará el SensorHilo más adelante
     }
 
-    public void actualizarPantalla() {
-        // TODO: Issue #3 - Implementar la actualización de la UI encapsulada de forma hilo-segura
-        // Requisitos:
-        // 1. Invocación obligatoria a Platform.runLater(() -> { ... });
-        // 2. Dentro del bloque seguro, extraer las métricas actualizadas del 'modelo' usando los getters.
-        // 3. Modificar los textos de labelTemp y labelHum.
-        // 4. Evaluar condicionalmente si (temperatura > 35 || humedad < 20). Si se cumple, cambiar el fondo
-        //    del panelAlerta a color ROJO (-fx-background-color: #d9534f;); de lo contrario, mantenerlo VERDE.
+    /**
+     * 3. Método preparado para actualizar la UI desde los hilos de manera segura
+     */
+    public void actualizarPantalla(double temperatura, int humedad) {
+        // Esto lo usaremos con Platform.runLater() para pintar los datos que vengan del SensorHilo
     }
 }
